e3ee5daddc3d40d9f4f6eca2bbc75bf0216157d5
Midori Takahashi <mintgreen7programming@gmail.com>
2023-03-22 05:40:56 +0900

    #13 ADD:ユーザー新規登録

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: "User was successfully created."
+      redirect_to login_path, notice: "User was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -53,6 +53,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
