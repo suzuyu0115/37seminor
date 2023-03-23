da1bfa89a5803dd0fd3de899fe08018405efe55e
mishina naoya <msnyxx811@gmail.com>
2023-03-23 22:08:31 +0900

    プロフィールのコントローラー、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,5 @@ Rails.application.routes.draw do
   resources :users
   resources :quests, only: %i[index]
   resources :password_resets, only: %i[new create edit update]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :profile, only: %i[show edit update]
 end
