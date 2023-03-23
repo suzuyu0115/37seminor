543581cda709264ce425c08e2d71bd140be247c0
Midori Takahashi <mintgreen7programming@gmail.com>
2023-03-23 14:40:36 +0900

    ADD:rails g controller PasswordResets create edit update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'password_resets/create'
+  get 'password_resets/edit'
+  get 'password_resets/update'
   get 'user_sessions/new'
   get 'user_sessions/create'
   get 'user_sessions/destroy'
