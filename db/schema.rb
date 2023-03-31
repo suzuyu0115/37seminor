cd2e54ae1be9a097c975a784a7709243675152ec
suzuyu0115 <zero000dia115@gmail.com>
2023-03-31 17:01:37 +0900

    Add: questsテーブルにstateカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_30_074508) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_075819) do
   create_table "joins", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "quest_id", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_30_074508) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "state", default: 0, null: false
     t.index ["user_id"], name: "index_quests_on_user_id"
   end
 
