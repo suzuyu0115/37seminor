1dca32fc385ec3ac86f97067761ce1ba6e15637c
mishina naoya <msnyxx811@gmail.com>
2023-04-02 19:23:38 +0900

    クエスト参加後、リダイレクト先を一覧に変更

--- app/controllers/joins_controller.rb
+++ app/controllers/joins_controller.rb
@@ -2,12 +2,12 @@ class JoinsController < ApplicationController
   def create
 		quest = Quest.find(params[:quest_id])
 		current_user.join(quest)
-		redirect_back fallback_location: quest
+		redirect_to quests_path
 	end
 
 	def destroy
 		quest = current_user.joins.find(params[:id]).quest
 		current_user.unjoin(quest)
-		redirect_back fallback_location: quest
+		redirect_to quests_path
 	end
 end
