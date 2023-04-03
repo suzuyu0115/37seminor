93b31c950e82a38f95d4e92b3b427261c949bcfd
Midori Takahashi <mintgreen7programming@gmail.com>
2023-04-03 13:48:07 +0900

    FIX:titleバリデーションno 変更、ロゴの追加"

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,9 +2,9 @@ class Quest < ApplicationRecord
   belongs_to :user
   has_many :joins, dependent: :destroy
 
-  validates :title, presence: true , length: { maximum: 12 }
+  validates :title, presence: true, length: { maximum: 8 }
   validates :step, presence: true
-  validates :body, presence: true, length: { maximum: 17 }
+  validates :body, presence: true, length: { maximum: 65535 }
   validates :place, presence: true, length: { maximum: 255 }
 
   enum step: { introduction: 0, basic: 1, advanced: 2, other: 3 }
