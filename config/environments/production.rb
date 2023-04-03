9fce54597d7c192c535270f24b18a1fcf6fe4402
Midori Takahashi <mintgreen7programming@gmail.com>
2023-04-03 16:47:02 +0900

    FIX:クエストタイトルの30文字バリデーション追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #パスワードリセットのための設定
+  cconfig.action_mailer.default_url_options = { protocol: 'https', host:'codehunter.magia.runteq.jp'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address:"smtp.gmail.com",
+    domain: 'gmail.com', #Gmailを使う場合
+    user_name: ENV['GMAIL_ADDRESS'], #Gmailアカウントのメールアドレス
+    password: ENV['GMAIL_PASSWORD'], #Gmailで設定したアプリパスワード
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
