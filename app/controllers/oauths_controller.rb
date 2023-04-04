294f9045b3d9222befb902817c51a8e1c19fc412
Midori Takahashi <mintgreen7programming@gmail.com>
2023-04-04 18:25:26 +0900

    FIX:環境変数の名前間違い訂正

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -15,7 +15,7 @@ class OauthsController < ApplicationController
         # 特定の組織にユーザーが所属しているかどうかを確認する
         # request.env['omniauth.auth']には、GitHubからの認証に関するすべての情報が含まれています。'extra'キーは、追加の情報を含むハッシュを表します。'raw_info'キーは、GitHub APIから返されるJSON形式のデータを表します。'organizations'キーは、ユーザーが所属している組織の配列を表します。'login'キーは、組織の名前を表します。各組織の'login'キーの値だけを取得するために、mapメソッドを使用しています。
         organizations = request.env['omniauth.auth']['extra']['raw_info']['organizations'].map { |org| org['login'] }
-        if organizations.include?('YOUR_ORGANIZATION_NAME')
+        if organizations.include?('ORGANIZATION_NAME')
           reset_session
           auto_login(@user)
           redirect_to quests_path, notice: "#{provider.titleize}でログインしました"
