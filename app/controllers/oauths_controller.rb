d617a9b59142e2f5d2b40bfdd61d89457b305800
Midori Takahashi <mintgreen7programming@gmail.com>
2023-04-04 17:06:56 +0900

    ADD:gem dotenv

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -6,12 +6,18 @@ class OauthsController < ApplicationController
   end
 
   def callback
-    provider = params[:provider]
-    if @user = login_from(provider)
-      redirect_to root_path, success: "#{provider.titleize}でログインしました。"
+    provider = auth_params[:provider]
+    if (@user = login_from(provider))
+      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
     else
-      session[:user_info] = @user_hash[:user_info]
-      redirect_to new_registration_path
+      begin
+        @user = create_from(provider)
+        reset_session
+        auto_login(@user)
+        redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      rescue StandardError
+        redirect_to root_path, alert: "#{provider.titleize}でのログインに失敗しました"
+      end
     end
   end
 
