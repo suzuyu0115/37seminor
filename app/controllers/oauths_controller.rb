106b40173fd59c3a9663cbc60cf65c2ede485c48
Midori Takahashi <mintgreen7programming@gmail.com>
2023-04-04 15:29:28 +0900

    FIX:

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -2,24 +2,22 @@ class OauthsController < ApplicationController
   skip_before_action :require_login, raise: false
 
   def oauth
-    return redirect_to root_path, info: t('defaults.info') if logged_in?
     login_at(params[:provider])
   end
 
   def callback
     provider = params[:provider]
     if @user = login_from(provider)
-      redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
+      redirect_to root_path, success: "#{provider.titleize}でログインしました。"
     else
-      begin
-        @user = create_from(provider)
-        # 注意：user_activationサブモジュールを使用している場合は、ここに「@user.activation!
-        reset_session # セッション固定化攻撃から保護する
-        auto_login(@user)
-        redirect_to root_path, success: "#{provider.titleize}アカウントでログインしました。"
-      rescue
-        redirect_to root_path, error: "#{provider.titleize}アカウントでのログインに失敗しました。"
-      end
+      session[:user_info] = @user_hash[:user_info]
+      redirect_to new_registration_path
     end
+  end
+
+  private
+
+  def auth_params
+    params.permit(:code, :provider, :error)
   end
 end
